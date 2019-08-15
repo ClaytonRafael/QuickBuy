216d4ef8fe3f38e004321cc4c3c4f933faa43ddf
claytonrafael4@gmail.com <claytonrafael4@gmail.com>
2019-08-14 21:34:33 -0300

    Criação de entidades.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public IFormatProvider FormaPagamento { get; set; }
+
+        //Deve ter pelo menos um item de pedido
+        //ou muitos itens de pedidos
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
